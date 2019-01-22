e572819b372bc4e3437587d427093ec52bbd5288
sanyapeng <sanyapeng@playcrab.com>
2019-01-22 09:12:07 +0800

    up

---
+++ python/Logging/demo01_基本使用.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#    File Name：       demo01_基本使用
+#    Description :
+#    Author :          SanYapeng
+#    date：            2019/1/21
+#    Change Activity:  2019/1/21:
+
+import  logging
+
+logging.basicConfig(level= logging.DEBUG, format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#logger = logging.getLogger(__name__)
+
+# logger.info("start print log")
+# logger.debug("Do sometings")
+# logger.warning("fail")
+# logger.info("finish")
+
+logging.info("pring info llog")
