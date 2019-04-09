b33cdd370faaa39b4eec5a0756fe918309ba33c6
San Yapeng <sanyapeng@playcrab.com>
2019-04-09 16:21:28 +0800

    delete

--- python/day19_多进程/demo02_process/__init__.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-#    File Name：       __init__.py
-#    Description :
-#    Author :          SanYapeng
-#    date：            2019-02-14
-#    Change Activity:  2019-02-14:
